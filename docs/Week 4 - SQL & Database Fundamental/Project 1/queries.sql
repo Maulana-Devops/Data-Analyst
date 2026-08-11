778f4d15820bc65831d70729c91c96d0f530bbb2
Maulana Aldi Pradana <maulanaaldipradana2008@gmail.com>
2026-08-11 12:37:24 +0700

    Create queries.sql

---
+++ docs/Week 4 - SQL & Database Fundamental/Project 1/queries.sql
@@ -0,0 +1,21 @@
+-- Project 01 - SQL Fundamentals
+
+-- Quiz 1: Select All Columns
+SELECT *
+FROM ms_produk;
+
+
+-- Quiz 2: Select One Column
+SELECT nama_produk
+FROM ms_produk;
+
+
+-- Quiz 3: Select Multiple Columns
+SELECT nama_produk, harga
+FROM ms_produk;
+
+
+-- Quiz 4: Limit Rows
+SELECT *
+FROM ms_produk
+LIMIT 5;
